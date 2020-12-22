f4e3eee2c1ae15754fc54d92fa8315e167bdd560
Mateus Felismino <45128866+mffdsp@users.noreply.github.com>
2020-12-21 23:34:34 -0300

    Add files via upload

--- Student.php
+++ Student.php
@@ -1,72 +1,47 @@
 <?php
 
 namespace App\Models;
+
 use PDO;
 
-class Student{
-
-    private $pdo;
-
-    public $CPF;
-    public $matricula;
-    public $email;
-    public $nome;
-    public $endereco;
+class Student {
 
     public function __CONSTRUCT()
 	{
-		try{
-            //$this->pdo = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=elpis;user=postgres;password=felismino");
-            //Instancia do postgre
-            return "ALUNOS CONSTRUTOR";
-
-		}catch(Exception $e){
-			die($e->getMessage());
-		}
+        $this->attributes = [
+            'nome' => [
+              'type' => 'string',
+              'validate' => 'required',
+              'size' => '50',
+            ],
+            'matricula' => [
+                'type' => 'string',
+                'validate' => 'required',
+                'size' => '50',
+              ],
+            'cpf' => [
+                'type' => 'string',
+                'validate' => 'required',
+                'size' => '50',
+              ],
+            'endereco' => [
+                'type' => 'string',
+                'validate' => 'required',
+                'size' => '50',
+            ],
+            'email' => [
+                'type' => 'string',
+                'validate' => 'required',
+                'size' => '50',
+              ],
+         ];
     }
 
-    public function setCPF($CPF){
-        $this->CPF = $CPF;
+    public function create($record){
+
+        $fields = array_keys($this->attributes);
+    
     }
-
-    public function getCPF(){
-       return $this->CPF;
-    }
-
-    public function setMatricula($matricula){
-        $this->matricula = $matricula;
-    }
-
-    public function getMatricula(){
-       return $this->matricula;
-    }
-
-    public function setEmail($email){
-        $this->email = $email;
-    }
-
-    public function getEmail(){
-       return $this->email;
-    }
-
-    public function setNome($nome){
-        $this->nome = $nome;
-    }
-
-    public function getNome(){
-       return $this->nome;
-    }
-
-    public function setEndereco($endereco){
-        $this->endereco = $endereco;
-    }
-
-    public function getEndereco(){
-       return $this->endereco;
-    }
-
-
-
 
 
 }
