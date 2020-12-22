3ec750cb258369885ac5d6ee2cba863c28015023
Mateus Felismino <45128866+mffdsp@users.noreply.github.com>
2020-12-21 23:34:25 -0300

    Add files via upload

--- StudentController.php
+++ StudentController.php
@@ -3,141 +3,94 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
+
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
 use PDO;
+
 
 //require_once 'App/Models/Student.php';
 
-class StudentController extends Controller
-{   
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
- 
-    public function index()
-    {
-        //
-    }
+class StudentController extends Controller{
+    
+    
+    protected $model;//??
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {   
-        $pdo = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=elpis;user=postgres;password=felismino");
-        $data = new Student();
-        // $data->nome = $_REQUEST['nome'];
-        // $data->endereco = $_REQUEST['endereco'];
-        // $data->cpf = $_REQUEST['cpf'];
-        // $data->email = $_REQUEST['email'];
-        // $data->matricula = $_REQUEST['matricula'];
-
-        //Example
-        $data->nome = "Mateus";
-        $data->endereco = "Cidade 01";
-        $data->cpf = "079797979";
-        $data->email = "something@example.com";
-        $data->matricula = "sdadsoakdok";
-
-        try {
-
-		$sqlCmd = "INSERT INTO Students (cpf, matricula, email, nome, endereco) VALUES (?, ?, ?, ?, ?)";
-
-		$pdo->prepare($sqlCmd)->execute(
-				array(
-                    $data->cpf, 
-                    $data->matricula,
-                    $data->email,
-                    $data->nome,
-                    $data->endereco,
-                )
-            );
-            
-            return('alunos');
-		} catch (Exception $e) {
-			die($e->getMessage());
-		}
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {   
-
-        $pdo = new PDO("pgsql:host=127.0.0.1;port=5432;dbname=elpis;user=postgres;password=felismino");
-        try {
-
-            
-            $sqlCmd = "SELECT * FROM Students WHERE cpf = :id";
-            $cmd = $pdo->prepare($sqlCmd);
-            
-            $stocks = [];
-            while ($row = $cmd->fetch(\PDO::FETCH_ASSOC)) {
-                $stocks[] = [
-                    'nome' => $row['nome'],
-                    'cpf' => $row['cpf'],
-                    'endereco' => $row['endereco']
-                ];
-            }
-            return $stocks;
-            
-        } catch (Exception $e) {
-			die($e->getMessage());
-		}
+    public function __construct(){
+        $this->model =  new Student;
         
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+    public function index(){
+        return view('alunos');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+    public function post(Request $request){
+        try{
+            $student = [
+                'cpf' => $_POST['cpf'],
+                'matricula' => $_POST['matricula'],
+                'email' => $_POST['email'],
+                'nome' => $_POST['nome'],
+                'endereco' => $_POST['endereco'],
+            ];
+            $this->model->create($student);
+
+            DB::table('students')->insert($student);
+            return $student;
+
+        }catch(Exception $e){
+            echo $e.getMessege();
+        }
+    }
+    
+    //Utilizo do slug, id, endpoint p acessar o bd.
+    public function get($id = null){
+
+        if($id == null){
+            $student = DB::table('students')->get();
+        }else {
+            $student = DB::table('students')->where('cpf', $id)->get();
+        }
+
+        return view('alunos', ['student' => $student]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+
+    public function put($id){
+        parse_str(file_get_contents('php://input'), $_PUT);
+
+        try{
+
+            $student = [
+                'cpf' => $_PUT['cpf'],
+                'matricula' => $_PUT['matricula'],
+                'email' => $_PUT['email'],
+                'nome' => $_PUT['nome'],
+                'endereco' => $_PUT['endereco'],
+            ];
+
+            DB::table('students')->where('cpf', $id)->update($student);
+
+            return $student;
+
+        }catch(Exception $e){
+            echo $e.getMessege();
+        }
+
+
     }
+
+    public function delete($id){
+        try{
+        $student = DB::table('students')->where('cpf', $id)->delete();
+        
+        return view('alunos', ['student' => $student]);
+        } catch(Exception $e){
+            echo $e.getMessage();
+
+        }
+    }
+   
 }
