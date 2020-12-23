d72ca02f52a67f3ebba25b6f8555fe388931c267
Mateus Felismino <45128866+mffdsp@users.noreply.github.com>
2020-12-23 00:01:59 -0300

    Add files via upload

--- TeacherController.php
+++ TeacherController.php
@@ -2,33 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Student;
+use App\Models\Teacher;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-//require_once 'App/Models/Student.php';
-
-class StudentController extends Controller{
-
+class TeacherController extends Controller{
+    
 
     public function index(){
-        return view('alunos', ['student' => null]);
+        return view('teachersView', ['teacher' => null]);
     }
 
     public function store(Request $request){
         try{
             
-            $student = [
+            $teacher = [
                 'cpf' => $request->cpf,
-                'matricula' =>  $request->matricula,
+                'turmas' =>  $request->turmas,
                 'email' => $request->email, 
                 'nome' => $request->nome,
-                'endereco' =>  $request->endereco,
             ];
             
-            Student::create($student);
-            return $student;
+            Teacher::create($teacher);
+            return $teacher;
 
         }catch(Exception $e){
             echo $e.getMessege();
@@ -38,14 +35,12 @@ class StudentController extends Controller{
     //Utilizo do slug, id, endpoint p acessar o bd.
     public function read($id = null){
 
-
         if($id == null){
-            $student = Student::get();
+            $teacher = Teacher::get();
         }else {
-            $student = Student::where('cpf', $id)->get();
-        }
-
-        return view('alunos', ['student' => $student]);
+            $teacher = Teacher::where('cpf', $id)->get();
+        }           
+        return view('teachersView', ['teacher' => $teacher]);
     }
 
 
@@ -54,17 +49,16 @@ class StudentController extends Controller{
 
         try{
 
-            $student = [
+            $teacher = [
                 'cpf' => $request->cpf,
-                'matricula' =>$request->matricula,
+                'turmas' =>$request->turmas,
                 'email' => $request->email,
                 'nome' => $request->nome,
-                'endereco' => $request->endereco,
             ];
 
-            $student = Student::where('cpf', $request->cpf)->update($student);
+            $teacher = Teacher::where('cpf', $request->cpf)->update($teacher);
 
-            return view('alunos', ['student' => $student]);
+            return view('teachersView', ['teacher' => $teacher]);
 
         }catch(Exception $e){
             echo $e.getMessege();
@@ -75,9 +69,9 @@ class StudentController extends Controller{
 
     public function delete($id){
         try{
-        $student = Student::where('cpf', $id)->delete();
+        $teacher = Teacher::where('cpf', $id)->delete();
         
-        return view('alunos', ['student' => $student]);
+        return view('teachersView', ['teacher' => $teacher]);
         } catch(Exception $e){
             echo $e.getMessage();
 
